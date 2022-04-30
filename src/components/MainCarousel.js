57a67c2a634a42781ad9c949d0dbf82599d4a65b
Moein_gh <moeinghorbanali.biz@gmail.com>
2022-04-30 21:30:03 +0430

    a

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { Carousel } from "react-bootstrap";
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
 
 const MainCarousel = () => {
   return (
-    <Carousel variant="dark" className="carouselContainer">
-      <Carousel.Item className="carouselItem">
+    <Carousel variant='dark' className='carouselContainer'>
+      <Carousel.Item className='carouselItem'>
         <img
-          className="d-block w-100 carouselImage"
-          src="/images/1.jpg"
-          alt="First slide"
+          className='d-block w-100 carouselImage'
+          src='/images/1.jpg'
+          alt='First slide'
         />
       </Carousel.Item>
-      <Carousel.Item className="carouselItem">
+      <Carousel.Item className='carouselItem'>
         <img
-          className="d-block w-100 carouselImage"
-          src="/images/2.jpg"
-          alt="Second slide"
+          className='d-block w-100 carouselImage'
+          src='/images/2.jpg'
+          alt='Second slide'
         />
       </Carousel.Item>
-      <Carousel.Item className="carouselItem">
+      <Carousel.Item className='carouselItem'>
         <img
-          className="d-block w-100 carouselImage"
-          src="/images/3.jpg"
-          alt="Third slide"
+          className='d-block w-100 carouselImage'
+          src='/images/3.jpg'
+          alt='Third slide'
         />
       </Carousel.Item>
     </Carousel>
-  );
-};
+  )
+}
 
-export default MainCarousel;
+export default MainCarousel
